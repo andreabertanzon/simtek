86ac42d59db060c2722669c5f2816ffcbea80891
andreabertanzon <andrea.bertanzon@protonmail.com>
2023-06-05 19:02:23 +0200

    starting from ground 0

--- backend/go.mod
+++
@@ -1,3 +0,0 @@
-module simtek-api
-
-go 1.19
--- frontend/src/models/site.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod"
-
-export const siteSchema = z.object({
-  Name: z.string().min(1),
-  Address: z.string().optional(),
-  City: z.string().optional(),
-  Cap: z.string().optional()
-})
-export type Site = z.infer<typeof siteSchema> 
