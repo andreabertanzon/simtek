d998f73f06834a77ce2eb799faa8f16258a78637
abcode <andrea.bertanzon@gmail.com>
2024-02-17 14:28:39 +0100

    fix(materials): fixes empty material being posted and not cleaned

--- models/intervention.go
+++ models/intervention.go
@@ -1,18 +1,5 @@
 package models
 
-import (
-	"strconv"
-	"strings"
-	"time"
-)
-
-type InterventionInput struct {
-	Site         string   `form:"site"`
-	Intervention string   `form:"intervention"`
-	Materials    []string `form:"materials[]"`
-	Workers      []string `form:"workers[]"`
-}
-
 type Intervention struct {
 	Site         string           `json:"site"`
 	Descriptions []string         `json:"descriptions"`
@@ -21,38 +8,7 @@ type Intervention struct {
 	Timestamp    string           `json:"timestamp"`
 }
 
-func (interventionInput *InterventionInput) ToDomainModel() Intervention {
-	var intervention Intervention
-	intervention.Site = interventionInput.Site
-	descriptions := strings.Split(interventionInput.Intervention, "\n")
-	intervention.Descriptions = descriptions
-
-	intervention.Materials = interventionInput.Materials
-	workers := make([]map[string]int, 0)
-	for _, worker := range interventionInput.Workers {
-		splittedWorker := strings.Split(worker, " ")
-		if len(splittedWorker) > 1 {
-			workerName := splittedWorker[0]
-			workerHours, err := strconv.Atoi(splittedWorker[1])
-			if err != nil {
-				workerHours = 0
-			}
-
-			workers = append(workers, map[string]int{workerName: workerHours})
-		} else if len(splittedWorker) == 1 {
-			workerName := splittedWorker[0]
-			workers = append(workers, map[string]int{workerName: 0})
-		} else {
-			workers = append(workers, map[string]int{"Simone": 0})
-		}
-	}
-
-	intervention.Workers = workers
-	intervention.Timestamp = time.Now().Format("02-01-2006:15:04:05")
-
-	return intervention
-}
-
+// Returns the total hours of the intervention
 func (intervention *Intervention) TotalHours() int {
 	totalHours := 0
 	for _, worker := range intervention.Workers {
---
+++ models/interventionInput.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"strconv"
+	"strings"
+	"time"
+)
+
+type InterventionInput struct {
+	Site         string   `form:"site"`
+	Intervention string   `form:"intervention"`
+	Materials    []string `form:"materials[]"`
+	Workers      []string `form:"workers[]"`
+}
+
+// Converts the InterventionInput to a Intervention domain model
+func (interventionInput *InterventionInput) ToDomainModel() Intervention {
+	var intervention Intervention
+	intervention.Site = interventionInput.Site
+	descriptions := strings.Split(interventionInput.Intervention, "\n")
+	intervention.Descriptions = descriptions
+
+	for _, material := range interventionInput.Materials {
+		if material == "" {
+			continue
+		}
+		intervention.Materials = append(intervention.Materials, material)
+	}
+
+	workers := make([]map[string]int, 0)
+	for _, worker := range interventionInput.Workers {
+		splittedWorker := strings.Split(worker, " ")
+		if len(splittedWorker) > 1 {
+			workerName := splittedWorker[0]
+			if workerName == "" {
+				workerName = "Simone"
+			}
+			workerHours, err := strconv.Atoi(splittedWorker[1])
+			if err != nil {
+				workerHours = 0
+			}
+
+			workers = append(workers, map[string]int{workerName: workerHours})
+		} else if len(splittedWorker) == 1 {
+			workerName := splittedWorker[0]
+			if workerName == "" {
+				workerName = "Simone"
+			}
+			workers = append(workers, map[string]int{workerName: 0})
+		} else {
+			workers = append(workers, map[string]int{"Simone": 0})
+		}
+	}
+
+	intervention.Workers = workers
+	intervention.Timestamp = time.Now().Format("02-01-2006:15:04:05")
+
+	return intervention
+}
